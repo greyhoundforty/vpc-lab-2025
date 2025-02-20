41bb282ff805b940c64b85d56c6ef7a67aac44ae
Ryan Tiffany <ryantiffany@fastmail.com>
2025-02-20 05:06:25 -0600

    fix: completed security group and vnic creation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 from time import sleep
 import random
 import click
-from utils import vpc_client, get_group_id_by_name, create_vpc
+from utils import *
 from rich.live import Live
 from rich.panel import Panel
 from rich.progress import Progress, SpinnerColumn, BarColumn, TextColumn
@@ -50,16 +50,22 @@ if not ibmcloud_api_key:
     help="Prefix for the VPC resources",
 )
 @click.option(
-    "--ssh-key",
-    prompt="Name of an existing SSH key in the region.",
-    help="VPC SSH key name",
+    "--tailscale-tag",
+    prompt="Tailscale tag to add to authentication token.",
+    help="Tailscale tag",
 )
-@click.option(
-    "--dns-zone",
-    prompt="Name of the Private DNS zone to create",
-    help="DNS Zone name",
-)
-def main(resource_group, region, prefix, ssh_key, dns_zone):
+# @click.option(
+#     "--ssh-key",
+#     prompt="Name of an existing SSH key in the region.",
+#     help="VPC SSH key name",
+# )
+# @click.option(
+#     "--dns-zone",
+#     prompt="Name of the Private DNS zone to create",
+#     help="DNS Zone name",
+# )
+# ssh_key, dns_zone
+def main(resource_group, region, prefix, tailscale_tag):
     job_progress = Progress(
         "{task.description}",
         SpinnerColumn(),
@@ -67,18 +73,17 @@ def main(resource_group, region, prefix, ssh_key, dns_zone):
         TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
     )
     job1 = job_progress.add_task(
-        f"[green]Starting VPC deployment in the {region} region", total=3
+        f"[green]Starting VPC deployment in the {region} region", total=2
     )
-    job2 = job_progress.add_task(
-        "[orange]Creating Public Gateways and Subnets", total=3
-    )
-    job3 = job_progress.add_task("[orange]Configuring VPC Security Groups", total=2)
-    job4 = job_progress.add_task("[blue]Creating Tailscale token", total=1)
-    job5 = job_progress.add_task("[blue]Creating Tailscale compute instance", total=1)
-    job6 = job_progress.add_task("[yellow]Creating Private DNS Zone", total=2)
-    job7 = job_progress.add_task(
-        "[yellow]Creating Private DNS Custom Resolver", total=2
-    )
+    job2 = job_progress.add_task("[green]Creating Public Gateways", total=2)
+    job3 = job_progress.add_task("[green]Creating front and backend subnets", total=2)
+    job4 = job_progress.add_task("[blue]Creating Tailscale Security Group", total=2)
+    job5 = job_progress.add_task("[blue]Creating Tailscale device token", total=1)
+    job6 = job_progress.add_task("[blue]Creating Tailscale compute", total=1)
+    # job6 = job_progress.add_task("[yellow]Creating Private DNS Zone", total=2)
+    # job7 = job_progress.add_task(
+    #     "[yellow]Creating Private DNS Custom Resolver", total=2
+    # )
 
     total = sum(task.total for task in job_progress.tasks)
     overall_progress = Progress()
@@ -97,29 +102,69 @@ def main(resource_group, region, prefix, ssh_key, dns_zone):
 
     with Live(progress_table, refresh_per_second=10):
         # Job 1 - create vpc client, retrieve resource group id, create vpc in the region
-        vpc_client = vpc_client(ibmcloud_api_key, region)
+        client = vpc_client(ibmcloud_api_key, region)
+        resource_group_id = get_group_id_by_name(resource_group)
         job_progress.update(job1, advance=1)
-
-        job_progress.update(job1, advance=1)
-
+        response = create_vpc(client, resource_group_id, prefix)
         vpc_id = response["id"]
         job_progress.update(job1, advance=1)
 
         # Job 2 = create public gateways and subnets
         regional_zones = []
-        zones = vpc_client.list_region_zones(region).get_result()["zones"]
+        zones = client.list_region_zones(region).get_result()["zones"]
         for zone in zones:
             regional_zones.append(zone["name"])
         job_progress.update(job2, advance=1)
 
-        for zone in regional_zones:
-            create_public_gateways(vpc_client, vpc_id, zone, resource_group_id)
-            job_progress.update(job2, advance=1)
-            create_subnets(vpc_client, vpc_id, zone, resource_group_id)
-            job_progress.update(job2, advance=1)
+        all_frontend_subnets = []
+        all_backend_subnets = []
 
+        for zone in regional_zones:
+            pgw_response = create_public_gateways(
+                client, vpc_id, zone, resource_group_id, prefix
+            )
+            pg_id = pgw_response["id"]
+            job_progress.update(job2, advance=1)
+            frontend_subnet = create_subnets(
+                client, pg_id, resource_group_id, vpc_id, zone, f"{prefix}-frontend"
+            )
+            all_frontend_subnets.append(frontend_subnet["id"])
+            job_progress.update(job3, advance=1)
+            backend_subnet = create_subnets(
+                client, None, resource_group_id, vpc_id, zone, f"{prefix}-backend"
+            )
+            all_backend_subnets.append(backend_subnet["id"])
+            job_progress.update(job3, advance=1)
+
+        # job 4 create secrutiy group
+        ts_group_resonse = create_tailscale_sg_group(
+            client, vpc_id, resource_group_id, prefix
+        )
+        job_progress.update(job4, advance=1)
+        sg_id = ts_group_resonse["id"]
+        update_rules = create_rules(client, sg_id)
+        job_progress.update(job4, advance=1)
+        tailscale_device_token = create_tailscale_key(
+            tailscale_api_key, tailnet_id, tailscale_tag
+        )
+        job_progress.update(job5, advance=1)
+        first_subnet_id = all_frontend_subnets[0]
+        logger.info(f"Device Token ID: {tailscale_device_token['id']}")
+        # logger.info(f"Device Token: {tailscale_device_token['key']}")
+        logger.info(f"subnet info: {first_subnet_id}")
+
+        # first_zone_subnet_id = frontend_subnets["subnets"][0]["id"]
+        new_vnic = create_vnic(
+            client, first_subnet_id, resource_group_id, prefix, sg_id
+        )
+        job_progress.update(job6, advance=1)
+        logger.info(f"VNIC ID: {new_vnic['id']}")
         completed = sum(task.completed for task in job_progress.tasks)
         overall_progress.update(overall_task, completed=completed)
 
         while not overall_progress.finished:
             sleep(0.1)
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import os
-import subprocess
+import httpx
 from datetime import datetime
-import ibm_vpc
+from ibm_vpc import VpcV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_cloud_sdk_core.api_exception import ApiException
 from ibm_platform_services.resource_controller_v2 import *
@@ -41,7 +41,7 @@ def resource_manager_service():
 
 def vpc_client(ibmcloud_api_key, region):
     authenticator = IAMAuthenticator(ibmcloud_api_key)
-    service = ibm_vpc.VpcV1(authenticator=authenticator)
+    service = VpcV1(authenticator=authenticator)
     service.set_service_url(f"https://{region}.iaas.cloud.ibm.com/v1")
     return service
 
@@ -61,98 +61,174 @@ def get_group_id_by_name(resource_group_name):
     return None
 
 
-def create_vpc(vpc_client, resource_group_name, prefix):
-    resource_group_identity_model = {}
-    resource_group_identity_model["id"] = get_group_id_by_name(resource_group_name)
-    resource_group_id = resource_group_identity_model
-    vpc_name = f"{prefix}-vpc"
+def create_vpc(vpc_client, resource_group_id, prefix):
     address_prefix_management = "auto"
     response = vpc_client.create_vpc(
         classic_access=False,
-        address_prefix_management=address_prefix_management,
-        name=vpc_name,
-        resource_group=resource_group_id,
+        address_prefix_management="auto",
+        name=f"{prefix}-vpc",
+        resource_group={"id": resource_group_id},
     ).get_result()
     return response
 
 
 def create_public_gateways(vpc_client, vpc_id, zone_name, resource_group_id, prefix):
-    vpc_identity_model = {}
-    vpc_identity_model["id"] = vpc_id
-
-    zone_identity_model = {}
-    zone_identity_model["name"] = zone_name
-
-    resource_group_identity_model = {}
-    resource_group_identity_model["id"] = resource_group_id
-
-    vpc = vpc_identity_model
-    zone = zone_identity_model
-    name = f"{prefix}-pgw-{zone}"
-    resource_group = resource_group_identity_model
+    # Create models directly in the function call
     response = vpc_client.create_public_gateway(
-        vpc,
-        zone,
-        name=name,
-        resource_group=resource_group,
+        vpc={"id": vpc_id},
+        zone={"name": zone_name},
+        name=f"{prefix}-pgw-{zone_name}",
+        resource_group={"id": resource_group_id},
     ).get_result()
 
     return response
 
 
 def create_subnets(
-    vpc_client,
-    network_acl_id,
-    public_gateway_id,
-    resource_group_id,
-    vpc_id,
-    zone,
-    prefix,
+    vpc_client, public_gateway_id, resource_group_id, vpc_id, zone, prefix
 ):
-    network_acl_identity_model = {}
-    network_acl_identity_model["id"] = network_acl_id
-
-    public_gateway_identity_model = {}
-    public_gateway_identity_model["id"] = public_gateway_id
-
-    resource_group_identity_model = {}
-    resource_group_identity_model["id"] = resource_group_id
-
-    vpc_identity_model = {}
-    vpc_identity_model["id"] = vpc_id
-
-    zone_identity_model = {}
-    zone_identity_model["name"] = zone
-
-    subnet_prototype_model = {}
-    subnet_prototype_model["ip_version"] = "both"
-    subnet_prototype_model["name"] = f"{prefix}-subnet-{zoneName}"
-    subnet_prototype_model["network_acl"] = network_acl_identity_model
-    subnet_prototype_model["public_gateway"] = public_gateway_identity_model
-    subnet_prototype_model["resource_group"] = resource_group_identity_model
-    subnet_prototype_model["vpc"] = vpc_identity_model
-    subnet_prototype_model["total_ipv4_address_count"] = 128
-    subnet_prototype_model["zone"] = zone_identity_model
-
-    subnet_prototype = subnet_prototype_model
+    subnet_prototype = {
+        "ip_version": "ipv4",
+        "name": f"{prefix}-subnet-{zone}",
+        "public_gateway": {"id": public_gateway_id} if public_gateway_id else None,
+        "resource_group": {"id": resource_group_id},
+        "vpc": {"id": vpc_id},
+        "total_ipv4_address_count": 128,
+        "zone": {"name": zone},
+    }
 
     response = vpc_client.create_subnet(subnet_prototype).get_result()
     return response
 
 
-def create_security_groups(region):
-    service = "thing"
-    time.sleep(random.randint(2, 8))
-    return service
+def create_tailscale_sg_group(vpc_client, vpc_id, resource_group_id, prefix):
+    security_group = vpc_client.create_security_group(
+        vpc={"id": vpc_id},
+        name=f"{prefix}-security-group",
+        resource_group={"id": resource_group_id},
+    )
+    # security_group_prototype = {
+    #     "ip_version": "ipv4",
+    #     "name": f"{prefix}-security-group",
+    #     "resource_group": {"id": resource_group_id},
+    #     "vpc": {"id": vpc_id},
+    # }
+
+    response = security_group.get_result()
+    return response
 
 
-def create_tailscale_token(region):
-    service = "thing"
-    time.sleep(random.randint(2, 8))
-    return service
+def create_rules(vpc_client, sg_id):
+    security_group_rule_protocol_tcp_remote_model = {}
+    security_group_rule_protocol_tcp_remote_model["cidr_block"] = "100.64.0.0/10"
+    security_group_rule_prototype_model = {}
+    security_group_rule_prototype_model["direction"] = "inbound"
+    security_group_rule_prototype_model["ip_version"] = "ipv4"
+    security_group_rule_prototype_model["protocol"] = "icmp"
+    security_group_rule_prototype_model["code"] = 0
+    security_group_rule_prototype_model["type"] = 8
+    security_group_rule_prototype = security_group_rule_prototype_model
+    response = vpc_client.create_security_group_rule(
+        sg_id, security_group_rule_prototype
+    )
+
+    security_group_rule_prototype_model = {}
+    security_group_rule_prototype_model["direction"] = "inbound"
+    security_group_rule_prototype_model["ip_version"] = "ipv4"
+    security_group_rule_prototype_model["protocol"] = "tcp"
+    security_group_rule_prototype_model["port_min"] = 22
+    security_group_rule_prototype_model["port_max"] = 22
+    security_group_rule_prototype_model[
+        "remote"
+    ] = security_group_rule_protocol_tcp_remote_model
+    security_group_rule_prototype = security_group_rule_prototype_model
+    response = vpc_client.create_security_group_rule(
+        sg_id, security_group_rule_prototype
+    )
+
+    security_group_rule_prototype_model = {}
+    security_group_rule_prototype_model["direction"] = "inbound"
+    security_group_rule_prototype_model["ip_version"] = "ipv4"
+    security_group_rule_prototype_model["protocol"] = "tcp"
+    security_group_rule_prototype_model["port_min"] = 80
+    security_group_rule_prototype_model["port_max"] = 80
+    security_group_rule_prototype = security_group_rule_prototype_model
+    response = vpc_client.create_security_group_rule(
+        sg_id, security_group_rule_prototype
+    )
+
+    security_group_rule_prototype_model = {}
+    security_group_rule_prototype_model["direction"] = "inbound"
+    security_group_rule_prototype_model["ip_version"] = "ipv4"
+    security_group_rule_prototype_model["protocol"] = "tcp"
+    security_group_rule_prototype_model["port_min"] = 443
+    security_group_rule_prototype_model["port_max"] = 443
+    security_group_rule_prototype = security_group_rule_prototype_model
+    response = vpc_client.create_security_group_rule(
+        sg_id, security_group_rule_prototype
+    )
+
+    security_group_rule_prototype_model = {}
+    security_group_rule_prototype_model["direction"] = "outbound"
+    security_group_rule_prototype_model["ip_version"] = "ipv4"
+    security_group_rule_prototype_model["protocol"] = "all"
+    security_group_rule_prototype = security_group_rule_prototype_model
+    response = vpc_client.create_security_group_rule(
+        sg_id, security_group_rule_prototype
+    )
 
 
-def create_tailscale_compute(region):
-    service = "thing"
-    time.sleep(random.randint(2, 8))
-    return service
+def create_tailscale_key(token, tailnet_id, tailscale_tag):
+    url = f"https://api.tailscale.com/api/v2/tailnet/{tailnet_id}/keys?all=true"
+    headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+    data = {
+        "capabilities": {
+            "devices": {
+                "create": {
+                    "reusable": False,
+                    "ephemeral": True,
+                    "preauthorized": True,
+                    "tags": [f"{tailscale_tag}"],
+                }
+            }
+        },
+        "expirySeconds": 86400,
+        "description": "Labme access",
+    }
+
+    response = httpx.post(url, headers=headers, json=data)
+    return response.json()
+
+
+def create_vnic(vpc_client, subnet_id, resource_group_id, prefix, security_group_id):
+    allow_ip_spoofing = False
+    security_groups = [{"id": security_group_id}]
+
+    response = vpc_client.create_virtual_network_interface(
+        name=f"{prefix}-tailscale-vnic",
+        subnet={"id": subnet_id},
+        allow_ip_spoofing=allow_ip_spoofing,
+        resource_group={"id": resource_group_id},
+        security_groups=security_groups,
+    )
+    virtual_network_interface = response.get_result()
+
+    return virtual_network_interface
+
+
+# def create_security_groups(region):
+#     service = "thing"
+#     time.sleep(random.randint(2, 8))
+#     return service
+
+
+# def create_tailscale_token(region):
+#     service = "thing"
+#     time.sleep(random.randint(2, 8))
+#     return service
+
+
+# def create_tailscale_compute(region):
+#     service = "thing"
+#     time.sleep(random.randint(2, 8))
+#     return service
